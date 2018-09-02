f397c9ee781cd51f5bb3df067df65db21561e486
Dr. Steven P. Crain <scrai002@plattsburgh.edu>
2018-09-02 19:37:10 -0400

    Copied from HelloWorld

---
+++ optimization/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+/**
+ * Example simple application in C++.
+ */
+
+class HelloWorld
+{
+private:
+    std::string greeting;
+
+public:
+    HelloWorld():
+        greeting("Hello World...")
+    {
+    }
+
+    /**
+     * Greet the world.
+     *
+     * @return whether we were successful.
+     */
+    bool greet();
+};
+
+int main()
+{
+    HelloWorld greeter; // Create an instance of HelloWorld.
+
+    if(!greeter.greet())
+    {
+        std::cerr << "Error greeting :-(" << std::endl;
+        return 1; // Failure
+    }
+
+    return 0; // Success
+}
+
+bool HelloWorld::greet()
+{
+    // Greet the world.
+    std::cout << greeting << std::endl;
+    
+    return std::cout.good(); // Return whether we were successful.
+}
